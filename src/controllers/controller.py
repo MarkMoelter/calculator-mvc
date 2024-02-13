aadf84a96b742a1ccff9906daf0af9b5e5a95262
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-02-13 15:44:20 -0500

    feat: add MVC classes

---
+++ src/controllers/controller.py
@@ -0,0 +1,15 @@
+from src.models import Model
+from src.views import View
+
+
+class Controller:
+
+    def __init__(self, model: Model, view: View) -> None:
+        self.model = model
+        self.view = view
+
+    def start(self) -> None:
+        """
+        Start the application.
+        :return: None
+        """
--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+from .controllers import Controller
+from .models import Model
+from .views import View
+
+
 def main() -> None:
-    pass
+    model = Model()
+    view = View()
+    controller = Controller(model, view)
+
+    controller.start()
 
 
 if __name__ == '__main__':
