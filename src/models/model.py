bb5138833a5192431d96fb123361b80117e073e5
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-02-19 15:24:06 -0500

    feat: add support for inversion, adding a decimal, and deleting the last digit

--- src/models/model.py
+++ src/models/model.py
@@ -1,6 +1,6 @@
 import logging
 
-from calc_caption_enum import CalcCaption
+import calc_captions as cc
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +15,21 @@ class Model:
         if caption == "C":
             self.value = ""
 
-        elif caption == CalcCaption.INVERT_SIGN:
-            pass
+        elif caption == cc.INVERT_SIGN and self.value and self.value != cc.DECIMAL:
+            # Determine if int or float
+            if cc.DECIMAL in self.value:
+                numeric_val = float(self.value)
+            else:
+                numeric_val = int(self.value)
+
+            self.value = str(numeric_val * -1)
+
+        elif caption == cc.DECIMAL:
+            if caption not in self.value:
+                self.value += caption
+
+        elif caption == cc.DELETE:
+            self.value = self.value[:-1]
 
         elif caption.isdigit():
             self.value += caption
