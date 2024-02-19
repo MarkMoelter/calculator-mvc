ac33e5706e3a72648cf888fce939a9c9c45ec9cd
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-02-19 16:01:46 -0500

    test: add more tests

--- tests/test_model/test_calculate.py
+++ tests/test_model/test_calculate.py
@@ -2,10 +2,10 @@ import src.calc_captions as cc
 from src.models import Model
 
 
-def test_calculate_input_C_clears_value():
+def test_calculate_input_CLEAR_clears_value():
     model = Model()
 
-    model.value = "0"
+    model.value = "01234"
     model.calculate(cc.CLEAR)
     assert model.value == ""
 
@@ -32,3 +32,45 @@ def test_calculate_input_INVERT_float_value_stays_float():
     model.value = "1.0"
 
     assert "." in model.calculate(cc.INVERT)
+
+
+def test_calculate_input_DECIMAL_adds_decimal_point_at_end():
+    model = Model()
+
+    model.value = "1"
+
+    assert model.calculate(cc.DECIMAL) == "1."
+
+
+def test_calculate_input_DECIMAL_does_not_add_two_decimals():
+    model = Model()
+
+    model.value = "1"
+
+    model.calculate(cc.DECIMAL)
+    assert model.value == "1."
+
+    model.calculate(cc.DECIMAL)
+    assert model.value == "1."
+
+
+def test_calculate_input_DELETE_removes_last_digit():
+    model = Model()
+
+    model.value = "1234"
+
+    assert model.calculate(cc.DELETE) == "123"
+
+
+def test_calculate_input_DELETE_removes_decimal_at_end():
+    model = Model()
+
+    model.value = "123."
+
+    assert model.calculate(cc.DELETE) == "123"
+
+
+def test_calculate_input_1_appends_to_value():
+    model = Model()
+
+    assert model.calculate("1") == "1"
